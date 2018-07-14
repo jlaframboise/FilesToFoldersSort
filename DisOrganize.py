5e891167e5ae76427c707217b70a6669b2a5e56e
Jacob Laframboise <jlaframboise@cs.queensu.ca>
2018-07-13 22:02:42 -0400

    The tool will upgrade photos from the lowest level of each branch, will not carry them all the way up. Need to make ti so files are moved after directories are nested.

--- DisOrganize.py
+++ DisOrganize.py
@@ -1,13 +1,42 @@
 import os
 
+'''
 def unsortTree(path):
     os.chdir(path)
 
     for x in os.listdir():
         if os.path.isdir(x):
-            #unsortTree(os.path.join(path, x))
+            unsortTree(os.path.join(path, x))
+            pass
     for x in os.listdir():
         if os.path.isfile(x):
             filePath = os.path.join(path, x)
             destPath = os.path.join(os.path.split(path)[0], x)
             os.rename(filePath, destPath)
+'''
+
+def unsortTree(path):
+    os.chdir(path)
+    print('Running from {}'.format(path))
+
+    ogListDir = os.listdir()
+    for x in ogListDir:
+
+        if os.path.isdir(x):
+            unsortTree(os.path.join(path,x))
+            os.chdir(path)
+            print('Back in {}'.format(path))
+
+        else: # os.path.isfile(x):
+
+            filePath = os.path.join(path, x)
+            destPath = os.path.join(os.path.split(path)[0], x)
+            print('Moving: {} to {}'.format(filePath, destPath))
+            os.rename(filePath, destPath)
+
+
+
+
+testPath = r"C:\Users\jaker\Desktop\fileOrgTesting - Copy"
+
+unsortTree(testPath)
